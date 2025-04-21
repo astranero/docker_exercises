081ab5e36639953d531b542af9fcece66fffe8ae
astranero <esa.taheresa@gmail.com>
2025-04-21 19:41:34 +0300

    new push

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! I believe that it works! I am a belieber</h1>')
+  res.send('<h1>Hello World! I believe that it works!</h1>')
 })
 
 const PORT = 8080
